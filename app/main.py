e648afb706c15e38935cd4b6f5abeccb18e2a7fc
Steve <ceosteve83@gmail.com>
2025-08-20 12:29:32 +0300

    api with sqlachemy orm methodology to access and work on a database

--- app/main.py
+++ app/main.py
@@ -15,6 +15,7 @@ from .database import engine, get_db
 models.Base.metadata.create_all(bind=engine)
 
 
+
 app = FastAPI()
 
 
@@ -23,11 +24,13 @@ app = FastAPI()
 # not any other kind of data
 # title str, content str,
 # schema defined with pydantic model
-
 class Post(BaseModel): # inherit from the BaseModel class of pydantic module
     title: str
     content: str
     published: bool = True
+
+
+
 
 # block of code to connect to the database on the local machine 
 while True:
@@ -44,6 +47,8 @@ while True:
         time.sleep(3)
     
 
+
+
 my_posts = [ 
     {"title":"title of post 1","content":"content of post 1", "id":1},
     {"title":"favorite foods","content":"I like pizza", "id":2}
@@ -51,10 +56,15 @@ my_posts = [
 ]
 
 
+
+
 def find_post(id):
     for p in my_posts:
         if p['id'] == id:
             return p
+
+
+
 
 # helper function to find the index of a post by its id
 # this is useful for deleting a post
@@ -64,78 +74,117 @@ def find_index_post(id):
             return i
 
 
+
+
 # path operation is basically a decorator with a specific http method and a path
 @app.get("/") # path operation (route)
 def root(): # path operation functions (make them as descriptive as possible)
     return {"message": "welcome to my api@@"} #JSON language
 
-#test path operation
+
+
+
+ # SQLALCHEMY test path operation
 @app.get("/sqlalchemy")
 def test_post(db:Session=Depends(get_db)):
-    return{"status":"success"}
+    posts=db.query(models.Posts).all()
+    print(posts)
+    return{"status":posts}
+
+
+
 
 # get all posts from the api server
 @app.get("/posts")
-def get_posts():
-    cursor.execute("SELECT *FROM posts")
-    posts =cursor.fetchall()
+def get_posts(db:Session=Depends(get_db)):
+    posts=db.query(models.Posts).all()
+#    cursor.execute("SELECT *FROM posts")
+#    posts =cursor.fetchall()
+    
     return {"data":posts}
+
+
 
 
 # create a post in the api server using the post HTTP method
 @app.post("/posts", status_code=status.HTTP_201_CREATED) # includes a status code to display that post has been created
-def create_posts(post: Post): # this is the pydantic model that was created earlier using the BaseModel class
-    cursor.execute(
-        "INSERT INTO posts (title, content) VALUES (%s, %s) RETURNING *", 
-        (post.title,post.content)
-    )
-    new_post = cursor.fetchone()
-    conn.commit()
-
+def create_posts(post: Post, db:Session=Depends(get_db)): # this is the pydantic model that was created earlier using the BaseModel class
+#    cursor.execute(
+#        "INSERT INTO posts (title, content) VALUES (%s, %s) RETURNING *", 
+#        (post.title,post.content)
+#    )
+#    new_post = cursor.fetchone()
+#    conn.commit()
+    new_post=models.Posts(**post.dict())  #unpacked dictionary created under pydantic model
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"data":new_post} # kinda like JSON format of reading information from a database
 
 
 
+ 
 # specify the status code that should be returned to the front end in case a resource
 # is unavailable in the server
 # data extracted is stored as a pydantic model and each has a method called .dict
 # use best pratices and naming conventions 
 @app.get("/posts/{id}") #id field represents a path parameter
-def retrieve_post(id:int):
-   cursor.execute("SELECT * FROM posts WHERE id = %s", (id,))
-   post=cursor.fetchone()
-   if not post:
+def retrieve_post(id:int,db:Session=Depends(get_db)):
+#   cursor.execute("SELECT * FROM posts WHERE id = %s", (id,))
+#   post=cursor.fetchone()
+
+    post= db.query(models.Posts).filter(models.Posts.id==id).first() # avoid going over all entries looking for id even if it has been found
+
+    if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                              detail=f"post with id :{id} was not found")
-   return{"post detail":post}
+    return{"post detail":post}
+
+
 
 # delete a post with a specific id
 # status code 204 means that the request was successful but there is no content to return
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
+def delete_post(id: int,db:Session=Depends(get_db)):
 
-    cursor.execute("DELETE FROM posts WHERE id=%s RETURNING *", (id,))
-    delete_post= cursor.fetchone()
-    conn.commit()
+#    cursor.execute("DELETE FROM posts WHERE id=%s RETURNING *", (id,))
+#    delete_post= cursor.fetchone()
+#    conn.commit()
 
-    if delete_post is None:
+    post=db.query(models.Posts).filter(models.Posts.id==id)
+   
+    if post.first() is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found")
+
+    post.delete(synchronize_session=False)
+    db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)  
 # No content to return after deletion
 
+
+
+
 # update a post with a specific id
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
+def update_post(id: int, post: Post, db:Session=Depends(get_db)):
 
-    cursor.execute("UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s RETURNING *",(post.title,post.content,post.published, (id,)))
+#    cursor.execute("UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s RETURNING *",(post.title,post.content,post.published, (id,)))
 
-    updated_post=cursor.fetchone()
-    conn.commit() # save changes permanently to the database
+#    updated_post=cursor.fetchone()
+#    conn.commit() # save changes permanently to the database
+    
+    post_query=db.query(models.Posts).filter(models.Posts.id==id)
+    
+    updated_post = post_query.first()
+
     if updated_post is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found")
     
-    return {"data":updated_post}  # for postman 
+    post_query.update(post.dict(), synchronize_session=False)
+    db.commit()
+
+    return {"data":post_query.first()}  # for postman 
     
 
 
