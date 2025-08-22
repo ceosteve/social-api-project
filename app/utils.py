3f6967c4e98ee645aa7d9d2545edd65f01acb177
Steve <ceosteve83@gmail.com>
2025-08-22 10:49:24 +0300

    created a utils file to store utility functions

---
+++ app/utils.py
@@ -0,0 +1,13 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"],deprecated="auto") #telling passlib what is the hashing algorithims
+
+
+def hash(password:str):
+    return pwd_context.hash(password)
+
+
+def verify(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
