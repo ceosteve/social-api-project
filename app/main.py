7e3048431b646d423e2e7b09fba7d03d3a828030
Steve <ceosteve83@gmail.com>
2025-08-22 10:46:33 +0300

    integrated the login authorization logic

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ import time
 from sqlalchemy.orm import Session
 from . import models, schemas, utils
 from .database import engine, get_db
-from .routers import post, user
+from .routers import post, user, auth
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -67,6 +67,7 @@ def find_index_post(id):
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 # path operation is basically a decorator with a specific http method and a path
 @app.get("/") # path operation (route)
