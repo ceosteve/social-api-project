65aeb88eb3e9097a467a1b8d8ce491fc52b3a85d
Steve <ceosteve83@gmail.com>
2025-08-26 13:31:00 +0300

    added a database dependency to the get current user function

--- app/oauth2.py
+++ app/oauth2.py
@@ -4,11 +4,18 @@ from tarfile import data_filter
 from xml.dom import minicompat
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
-from . import schemas
+from . import schemas, database, models
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 
+
+# this is like a helper function to get the JWT token from the header of a request and validate it
 oauth2_scheme = OAuth2PasswordBearer("login")
+
+
+
+
 # this handles the logic of creating JWT tokens
 # secret key
 # algorithm we want to use
@@ -16,7 +23,7 @@ oauth2_scheme = OAuth2PasswordBearer("login")
 
 SECRET_KEY="09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 def create_access_token(data:dict):
     to_encode=data.copy()
@@ -37,15 +44,19 @@ def verify_access_token(token:str, credentials_exception):
 
         if id is None:
             raise credentials_exception
-        token_data = schemas.TokenData(id)
+        token_data = schemas.TokenData()
     except JWTError:
         raise credentials_exception
     
     return token_data
 
-def get_current_user(token:str = Depends(oauth2_scheme)):
+def get_current_user(token:str = Depends(oauth2_scheme),db:Session=Depends(database.get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not verify credentials", headers={"www-Authenticate":"Bearer"})
 
-    return verify_access_token(token, credentials_exception)
+
+    token = verify_access_token(token,credentials_exception)
+    user=db.query(models.User).filter(models.User.id==token.id).first()
+
+    return user
 
 
