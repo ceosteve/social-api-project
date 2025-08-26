c2bc549d03999061f6de3f1697c7eae766bc958a
Steve <ceosteve83@gmail.com>
2025-08-26 23:21:01 +0300

    added a column for owner id in the posts sqlalchemy model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 
@@ -13,6 +13,7 @@ class Posts(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    owner_id = Column(Integer,ForeignKey("users.id", ondelete="CASCADE", onupdate="CASCADE"), nullable=False)
 
 
 # This is a SQLALCHEMY model that will define how user information to be stored in our database looks like.
