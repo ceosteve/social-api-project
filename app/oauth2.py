688a860bda596473f6dba872bfcf44a37c72d05b
Steve <ceosteve83@gmail.com>
2025-08-26 22:03:43 +0300

    changed the get_current_user function to fix a bug on database querrying for specific user

--- app/oauth2.py
+++ app/oauth2.py
@@ -35,14 +35,15 @@ def create_access_token(data:dict):
 
     return encoded_jwt
 
-def verify_access_token(token:str, credentials_exception):
+
+#def verify_access_token(token:str, credentials_exception):
     
     try:
         payload=jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
-        id:str=payload.get("user_id")
+        user_id:str=payload.get("user_id")
 
-        if id is None:
+        if user_id is None:
             raise credentials_exception
         token_data = schemas.TokenData()
     except JWTError:
@@ -50,13 +51,23 @@ def verify_access_token(token:str, credentials_exception):
     
     return token_data
 
-def get_current_user(token:str = Depends(oauth2_scheme),db:Session=Depends(database.get_db)):
-    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not verify credentials", headers={"www-Authenticate":"Bearer"})
 
 
-    token = verify_access_token(token,credentials_exception)
-    user=db.query(models.User).filter(models.User.id==token.id).first()
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("user_id")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
 
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user is None:
+        raise credentials_exception
     return user
-
-
