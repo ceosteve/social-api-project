0582e19a448f3c2026be1eb8ffb2cecd29aa291f
Steve <ceosteve83@gmail.com>
2025-08-27 18:40:04 +0300

    added a sqlalechemy model to create a posts table with foreign keys that point to id columns of the posts and users tables

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.orm import relationship
+
+from app.routers import post
 from .database import Base
 
 class Posts(Base):
@@ -26,3 +28,8 @@ class User(Base):
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+class Votes(Base):
+    __tablename__ = "votes"
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
