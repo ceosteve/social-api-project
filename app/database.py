5f79f7138b351cfd18c6b4aeb9839610d5f6b45b
Steve <ceosteve83@gmail.com>
2025-08-27 16:09:56 +0300

    modified the database url to use environment variables rather than hardcoding the values in the source code

--- app/database.py
+++ app/database.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+import psycopg
+from psycopg.rows import dict_row
+import time
+from .config import settings
 #databse connection string to be passed into SQLALCHEMY
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://postgres:postgres254@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{settings.database_username}:{
+    settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
                         # specify that it is a psotgres database, give username, password, ip address and database name
 
 
@@ -26,3 +30,18 @@ def get_db():
         db.close()
 
 
+# block of code to connect to the database on the local machine using SQL
+# 
+#while True:
+#    try:  
+#        conn = psycopg.connect("dbname=fastapi user=postgres password=postgres254 host=localhost")
+       
+#        cursor= conn.cursor(row_factory=dict_row) # to execute SQL statements
+ #      print("Database connection successful")
+#        break
+
+#    except Exception as error:
+#        print("Database connection failed")
+#        print("error", error)
+#        time.sleep(3)
+    
