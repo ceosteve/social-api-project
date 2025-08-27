d03e4a4939da4135fd35b230b94935862e293eff
Steve <ceosteve83@gmail.com>
2025-08-27 16:12:40 +0300

    modified the JWT token logic to use environment variables rather than hardcoding it into the source code

--- app/oauth2.py
+++ app/oauth2.py
@@ -5,6 +5,7 @@ from . import schemas, database, models
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
+from .config import settings
 
 
 # this is like a helper function to get the JWT token from the header of a request and validate it
@@ -18,9 +19,9 @@ oauth2_scheme = OAuth2PasswordBearer("login")
 # algorithm we want to use
 # expiration time of the token
 
-SECRET_KEY="09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+SECRET_KEY= settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expiration_time
 
 def create_access_token(data:dict):
     to_encode=data.copy()
