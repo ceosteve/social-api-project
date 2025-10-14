729041b354cbf007d115bb167e408ec7117ff5d0
ceosteve <steven01njoroge@gmail.com>
2025-10-14 15:42:50 +0300

    added logging to the login endpoint

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,11 +1,12 @@
 import http
+import logging
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from.. database import get_db
 from .. import schemas, models, utils, oauth2
 
-
+logger =logging.getLogger("socialapi")
 
 router = APIRouter(
     tags=['authentication']
@@ -18,13 +19,16 @@ def login(user_credentials:OAuth2PasswordRequestForm= Depends(), db: Session=Dep
     user= db.query(models.User).filter(models.User.email==user_credentials.username).first()
    
     if not user:
+        logger.warning("user tried logging in with invalid credentials")
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid credentials")
     
     if not utils.verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid credentials")
+         logger.warning("user tried logging in with invalid credentials")
+         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid credentials")
     
     access_token= oauth2.create_access_token(data={"user_id":user.id})
 
+    logger.info(f"user with id {user.id} logged in")
     return{"access_token":access_token, "token_type":"bearer"}
 
 # creating authentication tokens
