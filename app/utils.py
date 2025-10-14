88feac8141e24c730df6a0bb3db760828242d0aa
ceosteve <steven01njoroge@gmail.com>
2025-10-14 10:48:11 +0300

    created a function to generate cache keys from request

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,7 @@
 
+import hashlib
+from sys import prefix
+from fastapi import Request
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"],deprecated="auto") #telling passlib what is the hashing algorithims
@@ -10,5 +13,23 @@ def hash(password:str):
 
 def verify(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+# function to generate cache keys from the request
+def make_cache_key(request:Request, prefix:str="cache") -> str:
+
+    path = request.url.path
+
+    params = sorted(request.query_params.items())
+    if params:
+        param_string = "&".join(f"{k}={v}" for k, v in params)
+    else:
+        param_string = "no_params"
+    
+    hashed_key = hashlib.md5(param_string.encode()).hexdigest()
+
+    return f"{path}:{hashed_key}"
+
+
+
 
 
